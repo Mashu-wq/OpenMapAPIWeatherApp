adcaf857beb4430a69b2dc3105a63685707adc04
Mayesha <mim2007088@stud.kuet.ac.bd>
2024-08-19 16:49:08 +0600

    initialized

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:cloudcast/screens/weatherScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // scaffoldBackgroundColor: Colors.black45,
+        useMaterial3: true,
+      ),
+      home: const WeatherScreen(),
+    );
+  }
+}
---
+++ lib/secrets.dart
@@ -0,0 +1,1 @@
+const String openAPIkeY = 'f4a5b348cbc4f315a57ec0926e59c743';
